4fa9dbed3419fc81f5afbf17aa1e35d62656c72e
Michael Kirk <michael.code@endoftheworl.de>
2018-06-01 12:56:12 -0400

    update to latest openssl

--- GRKOpenSSLFramework/OpenSSL-iOS/bin/openssl.framework/Headers/openssl.h
+++ GRKOpenSSLFramework/OpenSSL-iOS/bin/openssl.framework/Headers/openssl.h
@@ -2,7 +2,7 @@
 //  openssl.h
 //  OpenSSL-iOS
 //
-//  Created by @levigroker Tue Jan 16 14:43:27 EST 2018.
+//  Created by @levigroker Wed May 30 19:27:21 EDT 2018.
 //  Copyright © 2018 @levigroker. All rights reserved.
 //
 
