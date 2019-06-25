3dd7f18bfc7acbcc9acf7c8770d02c78ab5ed288
Nora Trapp <nora@signal.org>
2019-06-25 10:31:16 -0700

    Update pods

--- Target Support Files/SignalServiceKit/SignalServiceKit-umbrella.h
+++ Target Support Files/SignalServiceKit/SignalServiceKit-umbrella.h
@@ -18,7 +18,6 @@
 #import "PhoneNumber.h"
 #import "PhoneNumberUtil.h"
 #import "SignalAccount.h"
-#import "SignalAccountFinder.h"
 #import "SignalRecipient.h"
 #import "TSContactThread.h"
 #import "TSGroupThread.h"
