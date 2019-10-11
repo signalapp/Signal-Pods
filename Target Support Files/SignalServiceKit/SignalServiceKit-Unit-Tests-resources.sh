fa0f23517a859667210b4b680f4a405edfc4eb5c
Michael Kirk <michael@signal.org>
2019-10-11 10:56:13 -0700

    update pods

--- Target Support Files/SignalServiceKit/SignalServiceKit-Unit-Tests-resources.sh
+++ Target Support Files/SignalServiceKit/SignalServiceKit-Unit-Tests-resources.sh
@@ -109,6 +109,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/ias-root.cer"
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/SFSRootCAG2.crt"
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/textsecure.cer"
+  install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/schema.sql"
   install_resource "${PODS_ROOT}/SAMKeychain/Support/SAMKeychain.bundle"
 fi
 if [[ "$CONFIGURATION" == "App Store Release" ]]; then
@@ -124,6 +125,7 @@ if [[ "$CONFIGURATION" == "App Store Release" ]]; then
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/ias-root.cer"
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/SFSRootCAG2.crt"
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/textsecure.cer"
+  install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/schema.sql"
   install_resource "${PODS_ROOT}/SAMKeychain/Support/SAMKeychain.bundle"
 fi
 if [[ "$CONFIGURATION" == "Testable Release" ]]; then
@@ -139,6 +141,7 @@ if [[ "$CONFIGURATION" == "Testable Release" ]]; then
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/ias-root.cer"
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/SFSRootCAG2.crt"
   install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/Certificates/textsecure.cer"
+  install_resource "${PODS_ROOT}/../SignalServiceKit/Resources/schema.sql"
   install_resource "${PODS_ROOT}/SAMKeychain/Support/SAMKeychain.bundle"
 fi
 
