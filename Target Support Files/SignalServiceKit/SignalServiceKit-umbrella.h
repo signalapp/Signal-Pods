1554b903407fc36da98691d6a738598d248d1b6a
Nora Trapp <nora@signal.org>
2019-11-11 18:54:36 -0800

    Update pods

--- Target Support Files/SignalServiceKit/SignalServiceKit-umbrella.h
+++ Target Support Files/SignalServiceKit/SignalServiceKit-umbrella.h
@@ -97,6 +97,7 @@
 #import "OWSUnknownContactBlockOfferMessage.h"
 #import "OWSUnknownProtocolVersionMessage.h"
 #import "PreKeyBundle+jsonDict.h"
+#import "OWSReaction.h"
 #import "InstalledSticker.h"
 #import "KnownStickerPack.h"
 #import "StickerInfo.h"
