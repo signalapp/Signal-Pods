12b8b9c5db2b3a01dbda616e9895fec6c84652c7
Nora Trapp <nora@signal.org>
2020-01-02 17:15:06 -0800

    Production build of ZXingObjC 3.6.4

--- _Prebuild/Target Support Files/Pods-SignalShareExtension/Pods-SignalShareExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SignalShareExtension : NSObject
-@end
-@implementation PodsDummy_Pods_SignalShareExtension
-@end
--- _Prebuild/Target Support Files/Pods-SignalTests/Pods-SignalTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SignalTests : NSObject
-@end
-@implementation PodsDummy_Pods_SignalTests
-@end
