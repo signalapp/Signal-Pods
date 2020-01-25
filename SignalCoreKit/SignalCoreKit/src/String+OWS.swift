710da4f6b05819ea7f35f7e4c7d1058b8bc6ac75
Nora Trapp <nora@signal.org>
2020-01-24 21:40:23 -0800

    Update pods

--- SignalCoreKit/SignalCoreKit/src/String+OWS.swift
+++ SignalCoreKit/SignalCoreKit/src/String+OWS.swift
@@ -20,13 +20,24 @@ public extension String {
         return (self as NSString).filterStringForDisplay()
     }
 
+    // There appears to be a bug in NSBigMutableString that causes a
+    // crash when using prefix to (not) truncate long strings to their
+    // current length. safePrefix() avoids this crash by only using
+    // prefix() if necessary.
+    func safePrefix(_ maxLength: Int) -> String {
+        guard maxLength < count else {
+            return self
+        }
+        return String(prefix(maxLength))
+    }
+
     // Truncates string to be less than or equal to byteCount, while ensuring we never truncate partial characters for multibyte characters.
     func truncated(toByteCount byteCount: UInt) -> String? {
         var lowerBoundCharCount = 0
         var upperBoundCharCount = self.count
 
         while (lowerBoundCharCount < upperBoundCharCount) {
-            guard let upperBoundData = self.prefix(upperBoundCharCount).data(using: .utf8) else {
+            guard let upperBoundData = safePrefix(upperBoundCharCount).data(using: .utf8) else {
                 owsFailDebug("upperBoundData was unexpectedly nil")
                 return nil
             }
@@ -42,7 +53,7 @@ public extension String {
             }
 
             let midpointCharCount = (lowerBoundCharCount + upperBoundCharCount) / 2
-            let midpointString = self.prefix(midpointCharCount)
+            let midpointString = safePrefix(midpointCharCount)
 
             guard let midpointData = midpointString.data(using: .utf8) else {
                 owsFailDebug("midpointData was unexpectedly nil")
@@ -57,7 +68,7 @@ public extension String {
             }
         }
 
-        return String(self.prefix(upperBoundCharCount))
+        return String(safePrefix(upperBoundCharCount))
     }
 
     func replaceCharacters(characterSet: CharacterSet, replacement: String) -> String {
