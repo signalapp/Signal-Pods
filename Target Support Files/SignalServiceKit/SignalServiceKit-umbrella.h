104166ef8725a37ebbaa77f756f105747ce9b012
Matthew Chen <matthew@signal.org>
2020-06-07 12:06:33 -0300

    Update Cocoapods.

--- Target Support Files/SignalServiceKit/SignalServiceKit-umbrella.h
+++ Target Support Files/SignalServiceKit/SignalServiceKit-umbrella.h
@@ -142,7 +142,6 @@
 #import "SSKSignedPreKeyStore.h"
 #import "BaseModel.h"
 #import "SDSCrossProcess.h"
-#import "SDSDatabaseStorage+Objc.h"
 #import "SDSKeyValueStore+ObjC.h"
 #import "SSKAccessors+SDS.h"
 #import "OWSBroadcastMediaMessageJobRecord.h"
