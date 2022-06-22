a8b3f3f1dea5f2ff6099cb06035dbeb06c64e873
Max Radermacher <max@signal.org>
2022-06-22 16:08:51 -0700

    Update to RingRTC v2.20.10

--- Target Support Files/SignalRingRTC/SignalRingRTC-xcframeworks.sh
+++ Target Support Files/SignalRingRTC/SignalRingRTC-xcframeworks.sh
@@ -17,11 +17,11 @@ RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
 variant_for_slice()
 {
   case "$1" in
-  "WebRTC.xcframework/ios-arm64_x86_64-maccatalyst")
-    echo "maccatalyst"
-    ;;
   "WebRTC.xcframework/ios-arm64")
     echo ""
+    ;;
+  "WebRTC.xcframework/ios-arm64_x86_64-maccatalyst")
+    echo "maccatalyst"
     ;;
   "WebRTC.xcframework/ios-arm64_x86_64-simulator")
     echo "simulator"
@@ -32,11 +32,11 @@ variant_for_slice()
 archs_for_slice()
 {
   case "$1" in
-  "WebRTC.xcframework/ios-arm64_x86_64-maccatalyst")
-    echo "arm64 x86_64"
-    ;;
   "WebRTC.xcframework/ios-arm64")
     echo "arm64"
+    ;;
+  "WebRTC.xcframework/ios-arm64_x86_64-maccatalyst")
+    echo "arm64 x86_64"
     ;;
   "WebRTC.xcframework/ios-arm64_x86_64-simulator")
     echo "arm64 x86_64"
@@ -123,5 +123,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/../ThirdParty/WebRTC/Build/WebRTC.xcframework" "SignalRingRTC/WebRTC" "framework" "ios-arm64_x86_64-maccatalyst" "ios-arm64" "ios-arm64_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/../ThirdParty/WebRTC/Build/WebRTC.xcframework" "SignalRingRTC/WebRTC" "framework" "ios-arm64" "ios-arm64_x86_64-maccatalyst" "ios-arm64_x86_64-simulator"
 
