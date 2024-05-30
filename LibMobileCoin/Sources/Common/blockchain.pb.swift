55b27d49739883884027b590de463047d3a2cac1
Pete Walters <pete@signal.org>
2024-05-30 14:48:44 -0500

    Update Mobilecoin SDK

--- LibMobileCoin/Sources/Common/blockchain.pb.swift
+++ LibMobileCoin/Sources/Common/blockchain.pb.swift
@@ -752,7 +752,15 @@ extension Blockchain_BlockMetadata: SwiftProtobuf.Message, SwiftProtobuf._Messag
     var _nodeKey: External_Ed25519Public? = nil
     var _signature: External_Ed25519Signature? = nil
 
-    static let defaultInstance = _StorageClass()
+    #if swift(>=5.10)
+      // This property is used as the initial default value for new instances of the type.
+      // The type itself is protecting the reference to its storage via CoW semantics.
+      // This will force a copy to be made of this reference when the first mutation occurs;
+      // hence, it is safe to mark this as `nonisolated(unsafe)`.
+      static nonisolated(unsafe) let defaultInstance = _StorageClass()
+    #else
+      static let defaultInstance = _StorageClass()
+    #endif
 
     private init() {}
 
@@ -838,7 +846,15 @@ extension Blockchain_ArchiveBlockV1: SwiftProtobuf.Message, SwiftProtobuf._Messa
     var _signature: Blockchain_BlockSignature? = nil
     var _metadata: Blockchain_BlockMetadata? = nil
 
-    static let defaultInstance = _StorageClass()
+    #if swift(>=5.10)
+      // This property is used as the initial default value for new instances of the type.
+      // The type itself is protecting the reference to its storage via CoW semantics.
+      // This will force a copy to be made of this reference when the first mutation occurs;
+      // hence, it is safe to mark this as `nonisolated(unsafe)`.
+      static nonisolated(unsafe) let defaultInstance = _StorageClass()
+    #else
+      static let defaultInstance = _StorageClass()
+    #endif
 
     private init() {}
 
--- LibMobileCoin/Sources/Common/mistyswap_offramp.pb.swift
+++ LibMobileCoin/Sources/Common/mistyswap_offramp.pb.swift
@@ -834,7 +834,15 @@ extension MistyswapOfframp_Offramp: SwiftProtobuf.Message, SwiftProtobuf._Messag
     var _ongoingWithdrawalJson: String = String()
     var _balances: Dictionary<String,String> = [:]
 
-    static let defaultInstance = _StorageClass()
+    #if swift(>=5.10)
+      // This property is used as the initial default value for new instances of the type.
+      // The type itself is protecting the reference to its storage via CoW semantics.
+      // This will force a copy to be made of this reference when the first mutation occurs;
+      // hence, it is safe to mark this as `nonisolated(unsafe)`.
+      static nonisolated(unsafe) let defaultInstance = _StorageClass()
+    #else
+      static let defaultInstance = _StorageClass()
+    #endif
 
     private init() {}
 
